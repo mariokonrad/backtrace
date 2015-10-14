19698c3a679c43cdd153ab28194be6a9d5fbd4d9
Mario Konrad <mario.konrad@gmx.net>
2015-10-15 00:39:32 +0200

    README.md and LICENSE added

---
+++ README.md
@@ -0,0 +1,10 @@
+backtrace
+=========
+
+Output of backtrace, usable also from a signal handler (e.g. SIGSEGV).
+
+LICENSE
+-------
+
+See source files, or [LICENSE](LICENSE)
+
--- backtrace.cpp
+++ backtrace.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <signal.h>
 #include "backtrace.hpp"
+
+extern "C" void sig_handler(int)
+{
+	std::cout << "SIGGEGV catched\n";
+	std::cout << utils::stacktrace{} << "\n";
+	exit(1);
+}
 
 template <typename T> T h(T * p)
 {
-	std::cout << utils::stacktrace{} << "\n";
-	return *p;
+	if (p) {
+		std::cout << utils::stacktrace{} << "\n";
+		return T{};
+	} else {
+		return *p;
+	}
 }
 
 namespace example
@@ -16,9 +28,15 @@ int g(int * p) { return example::f(p); }
 
 int main(int, char **)
 {
+	signal(SIGSEGV, sig_handler);
+
+	std::cout << "Intentional output\n";
 	int a = 99;
 	int b = g(&a);
-
 	std::cout << "a=" << a << ", b=" << b << "\n";
+
+	int c = g(nullptr);
+	std::cout << "c=" << c << "\n";
+
 	return 0;
 }
